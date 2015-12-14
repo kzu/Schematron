5f4d762de6eec5f16ffc5f146471d09b38512f1b
Pedro Frederico <pedro.frederico@sky.uk>
2015-12-14 11:10:58 +0000

    Tweaked assembly configuration

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("NMatrix Schematron.NET")]
+[assembly: AssemblyDescription("A native .NET implementation of Schematron.")]
+[assembly: AssemblyDelaySign(false)]
---
+++ src/Properties/SharedGeneralAssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("NMatrix Project")]
+[assembly: AssemblyProduct("Schematron.NET")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyConfiguration("")]
